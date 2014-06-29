3629b055332f248d6dd0e30e01904b425fc92080
AJ Richardson <aj_r@shaw.ca>
2014-06-28 22:34:39 -0400

    Initial commit

---
+++ Command.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfHelpers
+{
+    /// <summary>
+    /// A basic implementation of the ICommand interface where the parameter is ignored.
+    /// </summary>
+    public class Command : ICommand
+    {
+        Action action;
+
+        public Command(Action a)
+        {
+            action = a;
+        }
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            action();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    /// A basic implementation of the ICommand interface that accepts a typed parameter.
+    /// </summary>
+    public class Command<T> : ICommand
+    {
+        Action<T> action;
+
+        public Command(Action<T> a)
+        {
+            action = a;
+        }
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            action((T)parameter);
+        }
+
+        #endregion
+    }
+}
---
+++ FilePathToNameConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfHelpers
+{
+    public class FilePathToNameConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var path = (string)value;
+            return Path.GetFileName(path);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+        #endregion
+    }
+}
