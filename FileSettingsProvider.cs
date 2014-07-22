12414648725b6ededc913d60f3d3d7cf3a223c6b
AJ Richardson <aj_r@shaw.ca>
2014-07-22 00:27:29 -0400

    Added custom settings providers that allow you to manually set the file location.

---
+++ FileSettingsProvider.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace WpfHelpers
+{
+    public abstract class FileSettingsProvider : SettingsProvider
+    {
+        protected Dictionary<string, string> ReplaceTokens { get; private set; }
+        private string defaultAppName;
+        private string location;
+
+        public FileSettingsProvider()
+        {
+            ReplaceTokens = new Dictionary<string, string>();
+            var entryAssembly = Assembly.GetEntryAssembly();
+            defaultAppName = entryAssembly.GetName().Name;
+            ApplicationName = defaultAppName;
+        }
+
+        public override sealed string ApplicationName
+        {
+            get
+            {
+                string appName;
+                if (!ReplaceTokens.TryGetValue("AppName", out appName))
+                {
+                    ReplaceTokens.Add("AppName", defaultAppName);
+                    appName = defaultAppName;
+                }
+                return appName;
+            }
+            set
+            {
+                if (ReplaceTokens.ContainsKey("AppName"))
+                    ReplaceTokens["AppName"] = defaultAppName;
+                else
+                    ReplaceTokens.Add("AppName", defaultAppName);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the settings storage location.
+        /// </summary>
+        public string Location
+        {
+            get
+            {
+                if (location != null)
+                    return location;
+                return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ApplicationName, "settings.xml");
+            }
+            set
+            {
+                location = value;
+            }
+        }
+        
+        public override void Initialize(string name, NameValueCollection config)
+        {
+            base.Initialize(ApplicationName, config);
+            if (config == null)
+                return;
+            string location = config["location"];
+            if (location != null)
+            {
+                Regex regex = new Regex(@"{(\w+)}");
+                var matches = regex.Replace(location, m =>
+                {
+                    var key = m.Groups[1].Value;
+                    Environment.SpecialFolder specialFolder;
+                    if (Enum.TryParse(key, true, out specialFolder))
+                    {
+                        return Environment.GetFolderPath(specialFolder);
+                    }
+                    string replacement;
+                    if (ReplaceTokens.TryGetValue(key, out replacement))
+                        return replacement;
+                    return m.Value;
+                });
+                Location = location;
+            }
+        }
+    }
+}
---
+++ XmlSettingsProvider.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace WpfHelpers
+{
+    public class XmlSettingsProvider : FileSettingsProvider
+    {
+        public override SettingsPropertyValueCollection GetPropertyValues(SettingsContext context, SettingsPropertyCollection collection)
+        {
+            var values = new SettingsPropertyValueCollection();
+            XDocument doc = null;
+            try
+            {
+                doc = XDocument.Load(Location);
+            }
+            catch
+            {
+                doc = new XDocument(new XElement("Settings"));
+            }
+            var root = doc.Element("Settings") ?? new XElement("Settings");
+            foreach (SettingsProperty setting in collection)
+            {
+                SettingsPropertyValue val = new SettingsPropertyValue(setting);
+                try
+                {
+                    var elem = root.Element(setting.Name);
+                    if (elem == null || elem.IsEmpty)
+                    {
+                        val.SerializedValue = setting.DefaultValue;
+                    }
+                    else if (setting.SerializeAs == SettingsSerializeAs.Xml)
+                    {
+                        val.SerializedValue = @"<?xml version=""1.0"" encoding=""utf-16""?>" + elem.FirstNode.ToString();
+                    }
+                    else
+                    {
+                        using (var reader = XmlReader.Create(new StringReader(elem.ToString())))
+                        {
+                            reader.MoveToContent();
+                            val.SerializedValue = reader.ReadElementContentAsString();
+                        }
+                    }
+                }
+                catch
+                {
+                    val.SerializedValue = setting.DefaultValue;
+                }
+                values.Add(val);
+            }
+            return values;
+        }
+
+        public override void SetPropertyValues(SettingsContext context, SettingsPropertyValueCollection collection)
+        {
+            var dir = Path.GetDirectoryName(Location);
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            using (var writer = new XmlTextWriter(Location, Encoding.UTF8))
+            {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Settings");
+
+                foreach (SettingsPropertyValue setting in collection)
+                {
+                    if (setting.UsingDefaultValue)
+                        continue;
+                    writer.WriteStartElement(setting.Name);
+                    if (setting.Property.SerializeAs == SettingsSerializeAs.Xml)
+                    {
+                        var rawXml = setting.SerializedValue.ToString();
+                        var xmlDeclaration = new Regex(@"^<\? *xml.*\?>");
+                        rawXml = xmlDeclaration.Replace(rawXml, string.Empty).Trim('\r', '\n');
+                        writer.WriteRaw(rawXml);
+                    }
+                    else
+                    {
+                        writer.WriteString(setting.SerializedValue.ToString());
+                    }
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.Flush();
+            }
+        }
+    }
+}
