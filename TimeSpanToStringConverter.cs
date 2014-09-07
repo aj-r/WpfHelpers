47813ed9a7dd6ab921e28d787e412ad2075d590b
AJ Richardson <aj_r@shaw.ca>
2014-09-07 01:15:19 -0400

    Added WindowExtensions and TimeSpanToStringConverter

---
+++ TimeSpanToStringConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfHelpers
+{
+    public class TimeSpanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TimeSpan))
+                throw new ArgumentException("Cannot convert  from type " + value.GetType().Name, "value");
+            var time = (TimeSpan)value;
+            if (time == TimeSpan.MaxValue)
+                return "never";
+            if (time.Milliseconds > 0)
+                return GetOutput((int)time.TotalMilliseconds, "millisecond", "milliseconds");
+            if (time.Seconds > 0)
+                return GetOutput((int)time.TotalSeconds, "second", "seconds");
+            if (time.Minutes > 0)
+                return GetOutput((int)time.TotalMinutes, "minute", "minutes");
+            if (time.Hours > 0)
+                return GetOutput((int)time.TotalHours, "hour", "hours");
+            return GetOutput((int)time.TotalDays, "day", "days");
+        }
+
+        private static string GetOutput(int value, string singular, string plural)
+        {
+            return value + " " + (value == 1 ? singular : plural);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is string))
+                throw new ArgumentException("Cannot convert back from type " + value.GetType().Name, "value");
+            var str = (string)value;
+            if (str == "never")
+                return TimeSpan.MaxValue;
+            // TODO: this is not tested. does it work?
+            var time = TimeSpan.Parse(str);
+            return time;
+        }
+    }
+}
---
+++ WindowExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+// From http://stackoverflow.com/questions/339620/how-do-i-remove-minimize-and-maximize-from-a-resizable-window-in-wpf
+namespace WpfHelpers
+{
+    public static class WindowExtensions
+    {
+        // from winuser.h
+        private const int GWL_STYLE = -16;
+        private const int WS_MAXIMIZEBOX = 0x10000;
+        private const int WS_MINIMIZEBOX = 0x20000;
+        private const int WS_SYSMENU = 0x80000;
+
+        [DllImport("user32.dll")]
+        extern private static int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        extern private static int SetWindowLong(IntPtr hwnd, int index, int value);
+
+        /// <summary>
+        /// Hides the minimize and maximize buttons.
+        /// </summary>
+        /// <param name="window">The window for which to hide the buttons.</param>
+        public static void HideMinimizeAndMaximizeButtons(this Window window)
+        {
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            var currentStyle = GetWindowLong(hwnd, GWL_STYLE);
+            SetWindowLong(hwnd, GWL_STYLE, (currentStyle & ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX));
+        }
+
+        /// <summary>
+        /// Hides the close button for the window. Also removes the icon and system menu from the top left corner.
+        /// </summary>
+        /// <param name="window">The window for which to hide the button.</param>
+        public static void HideCloseButton(this Window window)
+        {
+            IntPtr hwnd = new WindowInteropHelper(window).Handle;
+            var currentStyle = GetWindowLong(hwnd, GWL_STYLE);
+            SetWindowLong(hwnd, GWL_STYLE, (currentStyle & ~WS_SYSMENU));
+        }
+    }
+}
