203bde57ae0dc480cc2412d8aecd7a5c3de39e7f
AJ Richardson <aj_r@shaw.ca>
2014-11-07 19:36:34 -0500

    Added more options to TimeSpanToStringConverter

--- TimeSpanToStringConverter.cs
+++ TimeSpanToStringConverter.cs
@@ -13,24 +13,46 @@ namespace WpfHelpers
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (!(value is TimeSpan))
-                throw new ArgumentException("Cannot convert  from type " + value.GetType().Name, "value");
+                throw new ArgumentException("value must be a TimeSpan", "value");
             var time = (TimeSpan)value;
             if (time == TimeSpan.MaxValue)
                 return "never";
-            if (time.Milliseconds > 0)
-                return GetOutput((int)time.TotalMilliseconds, "millisecond", "milliseconds");
-            if (time.Seconds > 0)
-                return GetOutput((int)time.TotalSeconds, "second", "seconds");
-            if (time.Minutes > 0)
-                return GetOutput((int)time.TotalMinutes, "minute", "minutes");
-            if (time.Hours > 0)
-                return GetOutput((int)time.TotalHours, "hour", "hours");
-            return GetOutput((int)time.TotalDays, "day", "days");
-        }
 
-        private static string GetOutput(int value, string singular, string plural)
-        {
-            return value + " " + (value == 1 ? singular : plural);
+            DateTimeComponent component;
+            if (Component != DateTimeComponent.None)
+            {
+                component = Component;
+            }
+            else
+            {
+                string componentName = parameter as string;
+                if (!Enum.TryParse<DateTimeComponent>(componentName, out component))
+                {
+                    if (time.TotalDays >= 1.0)
+                        component = DateTimeComponent.Day;
+                    else if (time.TotalHours >= 1.0)
+                        component = DateTimeComponent.Hour;
+                    else if (time.TotalMinutes >= 1.0)
+                        component = DateTimeComponent.Minute;
+                    else if (time.TotalSeconds >= 1.0)
+                        component = DateTimeComponent.Second;
+                    else
+                        component = DateTimeComponent.Millisecond;
+                }
+            }
+            switch (component)
+            {
+                case DateTimeComponent.Day:
+                    return GetOutput((int)time.TotalDays, "day", "days", "day");
+                case DateTimeComponent.Hour:
+                    return GetOutput((int)time.TotalHours, "hour", "hours", "hr.");
+                case DateTimeComponent.Minute:
+                    return GetOutput((int)time.TotalMinutes, "minute", "minutes", "min.");
+                case DateTimeComponent.Second:
+                    return GetOutput((int)time.TotalSeconds, "second", "seconds", "sec.");
+                default:
+                    return GetOutput((int)time.TotalMilliseconds, "millisecond", "milliseconds", "ms");
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -44,5 +66,23 @@ namespace WpfHelpers
             var time = TimeSpan.Parse(str);
             return time;
         }
+
+        private string GetOutput(int value, string singular, string plural, string abbreviated)
+        {
+            return value + " " + (Abbreviate ? abbreviated : (value == 1 ? singular : plural));
+        }
+
+        public DateTimeComponent Component { get; set; }
+        public bool Abbreviate { get; set; }
+    }
+
+    public enum DateTimeComponent
+    {
+        None,
+        Day,
+        Hour,
+        Minute,
+        Second,
+        Millisecond
     }
 }
