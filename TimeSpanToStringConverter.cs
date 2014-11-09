356ee90fd295f0fac3301f2db177c220a07779b2
AJ Richardson <aj_r@shaw.ca>
2014-11-08 19:02:07 -0500

    Fixed ConvertBack implementation (however, note that TimeSpanToStringConverter.ConvertBack is not currently used anywhere)

--- TimeSpanToStringConverter.cs
+++ TimeSpanToStringConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Text.RegularExpressions;
 using System.Windows.Data;
 
 namespace WpfHelpers
@@ -10,6 +11,17 @@ namespace WpfHelpers
     [ValueConversion(typeof(TimeSpan), typeof(string))]
     public class TimeSpanToStringConverter : IValueConverter
     {
+        private static readonly string[,] componentNames = {
+            { null, null, null },
+            { "day", "days", null },
+            { "hour", "hours", "h" },
+            { "minute", "minutes", "min" },
+            { "second", "seconds", "s" },
+            { "millisecond", "milliseconds", "ms" }
+        };
+
+        private static readonly Regex regex = new Regex(@"([0-9]+) *(.+)");
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             if (!(value is TimeSpan))
@@ -26,7 +38,7 @@ namespace WpfHelpers
             else
             {
                 string componentName = parameter as string;
-                if (!Enum.TryParse<DateTimeComponent>(componentName, out component))
+                if (!Enum.TryParse(componentName, out component) || component == DateTimeComponent.None)
                 {
                     if (time.TotalDays >= 1.0)
                         component = DateTimeComponent.Day;
@@ -43,15 +55,15 @@ namespace WpfHelpers
             switch (component)
             {
                 case DateTimeComponent.Day:
-                    return GetOutput((int)time.TotalDays, "day", "days", "day");
+                    return GetOutput((int)time.TotalDays, component);
                 case DateTimeComponent.Hour:
-                    return GetOutput((int)time.TotalHours, "hour", "hours", "hr.");
+                    return GetOutput((int)time.TotalHours, component);
                 case DateTimeComponent.Minute:
-                    return GetOutput((int)time.TotalMinutes, "minute", "minutes", "min.");
+                    return GetOutput((int)time.TotalMinutes, component);
                 case DateTimeComponent.Second:
-                    return GetOutput((int)time.TotalSeconds, "second", "seconds", "sec.");
+                    return GetOutput((int)time.TotalSeconds, component);
                 default:
-                    return GetOutput((int)time.TotalMilliseconds, "millisecond", "milliseconds", "ms");
+                    return GetOutput((int)time.TotalMilliseconds, component);
             }
         }
 
@@ -62,14 +74,56 @@ namespace WpfHelpers
             var str = (string)value;
             if (str == "never")
                 return TimeSpan.MaxValue;
-            // TODO: this is not tested. does it work?
-            var time = TimeSpan.Parse(str);
-            return time;
+            var match = regex.Match(str);
+            if (match != null)
+            {
+                string valueString = match.Groups[1].Value;
+                string componentName = match.Groups[2].Value;
+                double componentValue;
+                if (componentName != null && double.TryParse(valueString, out componentValue))
+                {
+                    DateTimeComponent component = DateTimeComponent.None;
+                    var length0 = componentNames.GetLength(0);
+                    var length1 = componentNames.GetLength(1);
+                    for (int i = 0; i < length0; i++)
+                    {
+                        for (int j = 0; j < length1; j++)
+                        {
+                            if (componentName.Equals(componentNames[i, j], StringComparison.InvariantCultureIgnoreCase))
+                            {
+                                component = (DateTimeComponent)i;
+                                break;
+                            }
+                        }
+                        if (component != DateTimeComponent.None)
+                            break;
+                    }
+                    switch (component)
+                    {
+                        case DateTimeComponent.Day:
+                            return TimeSpan.FromDays(componentValue);
+                        case DateTimeComponent.Hour:
+                            return TimeSpan.FromHours(componentValue);
+                        case DateTimeComponent.Minute:
+                            return TimeSpan.FromMinutes(componentValue);
+                        case DateTimeComponent.Second:
+                            return TimeSpan.FromSeconds(componentValue);
+                        case DateTimeComponent.Millisecond:
+                            return TimeSpan.FromMilliseconds(componentValue);
+                    }
+                }
+            }
+            return TimeSpan.Parse(str);
         }
 
-        private string GetOutput(int value, string singular, string plural, string abbreviated)
+        private string GetOutput(int value, DateTimeComponent component)
         {
-            return value + " " + (Abbreviate ? abbreviated : (value == 1 ? singular : plural));
+            string componentName = null;
+            if (Abbreviate)
+                componentName = componentNames[(int)component, 2];
+            if(componentName == null)
+                componentName = componentNames[(int)component, value == 1 ? 0 : 1];
+            return string.Concat(value, " ", componentName);
         }
 
         public DateTimeComponent Component { get; set; }
