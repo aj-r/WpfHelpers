ccbb8b8b27691d0d4cbce1857e99943e9bf162b6
AJ Richardson <aj.ric.ar+git@gmail.com>
2015-07-09 20:04:04 -0400

    Added comment

--- DialogCloser.cs
+++ DialogCloser.cs
@@ -7,6 +7,11 @@ namespace Sharp.Utils.Wpf
     /// <summary>
     /// Contains an attached property that can be used to set the DialogResult of a window.
     /// </summary>
+    /// <remarks>
+    /// This attached property is necessary when you need to set the DialogResult from the view model.
+    /// Since the Window.DialogResult property is not a DependencyProperty, you cannot bind to it.
+    /// The DialogCloser.DialogResult property IS a DependencyProperty, so you can bind a ViewModel property to it.
+    /// </remarks>
     public static class DialogCloser
     {
         /// <summary>
